31e10c347d36d575d2939a4cac6e244c8632c3c9
Alex Tang <alextang@cisco.com>
2016-06-10 15:55:08 -0700

    Update build date and schema

--- csmserver/schema/base.py
+++ csmserver/schema/base.py
@@ -38,7 +38,7 @@ class BaseMigrate(object):
 
     def execute(self):
         self.start()
-        # self.update_schema_version()
+        self.update_schema_version()
 
     def start(self):       
         raise NotImplementedError("Children must override start")
