d14e24d3c059f9ea53dc1113a0e2c6f2efe21587
Alex Tang <alextang@cisco.com>
2016-09-06 16:28:01 -0700

    Fix server repository issue

--- csmserver/static/jquery/js/smu_info_dialog.js
+++ csmserver/static/jquery/js/smu_info_dialog.js
@@ -78,27 +78,31 @@ function display_smu_details(table, title, smu_id) {
         dataType: 'json',
         success: function(data) {
             $.each(data, function(index, element) {
-                var html = '';
-                html += create_html_table_row('SMU ID', element[0].id);
-                html += create_html_table_row('SMU Name', element[0].name);
-                html += create_html_table_row('Posted Date', element[0].posted_date);
-                html += create_html_table_row('Type', element[0].type);
-                html += create_html_table_row('Impact', element[0].impact);
-                html += create_html_table_row('Functional Areas', element[0].functional_areas);
-                html += create_html_table_row('DDTS', element[0].ddts);
-                html += create_html_table_row('Description', element[0].description);
-                html += create_html_table_row('Status', element[0].status);
-                html += create_html_table_row('Compressed Image Size', element[0].compressed_image_size);
-                html += create_html_table_row('Uncompressed Image Size', element[0].uncompressed_image_size);
-                html += create_html_table_row('Package Bundles', element[0].package_bundles);
-                html += create_html_table_row('Constituent DDTS', element[0].composite_DDTS);
-                html += create_hyperlink_html_table_row('Pre-requisites', element[0].prerequisites_smu_ids, element[0].prerequisites);
-                html += create_hyperlink_html_table_row('Supersedes', element[0].supersedes_smu_ids, element[0].supersedes);
-                html += create_hyperlink_html_table_row('Superseded By', element[0].superseded_by_smu_ids, element[0].superseded_by);
+                if (element.length == 0) {
+                    title.text('Unable to locate SMU ID: ' + smu_id)
+                    table.html('');
+                } else {
+                    var html = '';
+                    html += create_html_table_row('SMU ID', element[0].id);
+                    html += create_html_table_row('SMU Name', element[0].name);
+                    html += create_html_table_row('Posted Date', element[0].posted_date);
+                    html += create_html_table_row('Type', element[0].type);
+                    html += create_html_table_row('Impact', element[0].impact);
+                    html += create_html_table_row('Functional Areas', element[0].functional_areas);
+                    html += create_html_table_row('DDTS', element[0].ddts);
+                    html += create_html_table_row('Description', element[0].description);
+                    html += create_html_table_row('Status', element[0].status);
+                    html += create_html_table_row('Compressed Image Size', element[0].compressed_image_size);
+                    html += create_html_table_row('Uncompressed Image Size', element[0].uncompressed_image_size);
+                    html += create_html_table_row('Package Bundles', element[0].package_bundles);
+                    html += create_html_table_row('Constituent DDTS', element[0].composite_DDTS);
+                    html += create_hyperlink_html_table_row('Pre-requisites', element[0].prerequisites_smu_ids, element[0].prerequisites);
+                    html += create_hyperlink_html_table_row('Supersedes', element[0].supersedes_smu_ids, element[0].supersedes);
+                    html += create_hyperlink_html_table_row('Superseded By', element[0].superseded_by_smu_ids, element[0].superseded_by);
 
-                title.text('SMU Name: ' + element[0].name);
-                table.html(html);
-
+                    title.text('SMU Name: ' + element[0].name);
+                    table.html(html);
+                }
             });
         }
     });
