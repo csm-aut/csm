850262fbb5d259afeb04ba5dca350fa0a37f2737
Joy Dai <yijoydai@gmail.com>
2017-05-23 11:29:04 -0700

    refactor code to allow flexible platform defined monitor job scheduling. TO DO: need to remove testing code.

---
+++ csmserver/work_units/install_work_unit/platforms/IOS_XE.py
@@ -0,0 +1,36 @@
+# =============================================================================
+# Copyright (c) 2016, Cisco Systems, Inc
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+# Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+# THE POSSIBILITY OF SUCH DAMAGE.
+# =============================================================================
+from base import InstallPendingMonitoringWorkUnit
+from constants import JobStatus, InstallAction
+
+
+class IOSXEInstallPendingMonitoringWorkUnit(InstallPendingMonitoringWorkUnit):
+
+    # install action mapped against a list with 3 items:
+    # 1. install action for the monitor_job
+    # 2. time interval before next execution of the monitor job
+    # 3. max number of trials before we stop running the monitor job and declare failure of install job
+    monitor_info = {InstallAction.INSTALL_ADD: [InstallAction.ADD_MONITOR, 900, 10],
+                    InstallAction.INSTALL_ACTIVATE: [InstallAction.ACTIVATE_MONITOR, 1500, 3]}
