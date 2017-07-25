610b29dee008811b4cee21931cc0a071dd089561
Alex Tang <alextang@cisco.com>
2017-07-25 16:41:04 -0700

    Adjust schema version

---
+++ csmserver/schema/migrate_to_version_10.py
@@ -0,0 +1,57 @@
+# =============================================================================
+# Copyright (c) 2015, Cisco Systems, Inc
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+# Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+# THE POSSIBILITY OF SUCH DAMAGE.
+# =============================================================================
+from schema.base import BaseMigrate
+from database import DBSession
+
+from models import InstallJob
+from models import InventoryJob
+from models import DownloadJob
+from constants import JobStatus
+
+sql_statements = [
+    'alter table install_job add job_type VARCHAR(20)',
+    'drop table smu_info',
+    'drop table smu_meta',
+    'update install_job set install_action="Pre-Check" where install_action="Pre-Upgrade"',
+    'update install_job set install_action="Post-Check" where install_action="Post-Upgrade"',
+    'update install_job_history set install_action="Pre-Check" where install_action="Pre-Upgrade"',
+    'update install_job_history set install_action="Post-Check" where install_action="Post-Upgrade"',
+    'alter table inventory modify hardware_revision VARCHAR(30)',
+    'alter table host_inventory modify hardware_revision VARCHAR(30)'
+    ]
+
+
+class SchemaMigrate(BaseMigrate):
+    def __init__(self, version):
+        BaseMigrate.__init__(self, version)
+
+    def start(self):
+        db_session = DBSession()
+
+        for sql in sql_statements:
+            try:
+                db_session.execute(sql)
+            except Exception as e:
+                pass
